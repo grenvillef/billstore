b20d917b57cb98cb8769cc937d459159ee9781d9
root <root@ip-172-31-46-135.eu-west-3.compute.internal>
2018-05-09 21:52:10 +0000

    Clean up

--- server.js
+++
@@ -1,9 +0,0 @@
-var express = require('express'),
-
- app = express(),
- port = process.env.PORT || 3000;
-
-app.listen(port);
-
-console.log('getInvoices RESTful API server started on: ' + port);
-
